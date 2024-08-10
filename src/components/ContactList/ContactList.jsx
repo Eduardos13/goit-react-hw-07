dd4a7571ae701ac8ccc502bece007dd8f1be8bbb
Eduardos13 <edik.kozelsky@gmail.com>
2024-08-09 23:35:00 -0400

    some commit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import s from './ContactList.module.css';
-import { useSelector } from 'react-redux';
-import { selectContacts } from '../../redux/contacts/contactsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectContacts,
+  selectIsLoading,
+} from '../../redux/contacts/contactsSlice';
 import Contact from '../Contact/Contact';
 import { selectFilter } from '../../redux/contacts/filtersSlice';
+import { fetchContactsThunk } from '../../redux/contacts/contactsOps';
 
 const ContactList = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContactsThunk());
+  }, [dispatch]);
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectFilter);
 
@@ -15,11 +24,16 @@ const ContactList = () => {
 
   return (
     <ul className={s.contactsList}>
-      {filteredData.map((item) => (
-        <li key={item.id}>
-          <Contact item={item} />
-        </li>
-      ))}
+      {filteredData.length ? (
+        filteredData.map((item) => (
+          <li key={item.id}>
+            <Contact item={item} />
+          </li>
+        ))
+      ) : (
+        <h2>No contacts added</h2>
+      )}
+      {isLoading && <h2>Loading...</h2>}
     </ul>
   );
 };
--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,22 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
+import {
+  addContactThunk,
+  deleteContactThunk,
+  fetchContactsThunk,
+} from './contactsOps';
 
 const initialState = {
   items: [],
+  isLoading: false,
+  isError: false,
 };
 
 const slice = createSlice({
   name: 'contacts',
   initialState,
-  reducers: {
-    addContact: (state, action) => {
-      state.items.push(action.payload);
-    },
-    deleteContact: (state, action) => {
-      state.items = state.items.filter((item) => item.id !== action.payload);
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContactsThunk.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchContactsThunk.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteContactThunk.fulfilled, (state, action) => {
+        state.items = state.items.filter((item) => item.id !== action.payload);
+      })
+      .addCase(addContactThunk.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+      });
   },
 });
 
 export const contactsReducer = slice.reducer;
-export const { deleteContact, addContact } = slice.actions;
 export const selectContacts = (state) => state.contacts.items;
+export const selectIsLoading = (state) => state.contacts.isLoading;
+export const selectIsError = (state) => state.contacts.isError;
